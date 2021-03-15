1664c9c0e8a63d0350087ea0238663624ee893fd
Pradumna <pradumnabajoria@gmail.com>
2021-03-15 21:41:42 +0530

    learning to fetch data from api

--- general/src/App.js
+++ general/src/App.js
@@ -1,83 +1,33 @@
-import React from "react"
-import Conditional from "./Conditional"
-/*
-class App extends React.Component{
+import React, {Component} from "react"
+
+class App extends Component{
   constructor(){
     super()
     this.state = {
-      isLoading: true
+      loading: false,
+      character: {}
     }
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        isLoading: false
-      })
-    }, 1500)
-  }
-  
-  render(){
-    return(
-      <div>
-        <Conditional isLoading={this.state.isLoading}/>
-      </div>
-    )
-  }
-}
-
-export default App*/
-
-/*class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      unreadMessages: [
-        "Call Me!",
-        "You have new Message"
-      ]
-    }
-  }
-  
-  render(){
-    return(
-      <div>
-        {
-          this.state.unreadMessages.length > 0 && 
-          <h1>You have {this.state.unreadMessages.length} unread Message</h1>
-        }
-      </div>
-    )
-  }
-}
-*/
-
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      isLoggedIn: true
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick(){
-    console.log("Working")
-    this.setState((prevState) => {
-      return{
-        isLoggedIn: !prevState.isLoggedIn
-      }
-      
+    this.setState({
+      loading: true
     })
-    //this.setState({isLoggedIn: false})
+    fetch("https://swapi.dev/api/people/1/")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          character: data,
+          loading: false
+        })
+        //this.setState({loading: false})
+      })
   }
 
   render(){
-    console.log(this.state.isLoggedIn)
     return(
       <div>
-        {this.state.isLoggedIn ? <h1>Logged In</h1> : <h1>Logged Out</h1>}
-        <button onClick={this.handleClick}>{this.state.isLoggedIn ? "Log Out" : "Log In"}</button>
+        {this.state.loading ? "Loading......." : this.state.character.name}
       </div>
     )
   }
